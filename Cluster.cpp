83b0e3a3ab155fd76392b6ebf29660db2ffcd871
hoangk <Khoa.Hoang@Ucdenver.edu>
2016-03-02 23:01:27 -0700

    cluster cpp
    Just implement
    but I have to comment it because it gives many error.

---
+++ Cluster.cpp
@@ -0,0 +1,68 @@
+//
+// Created by Khoa on 2/25/16.
+//
+
+#include "Cluster.h"
+#include "Point.h"
+
+using namespace Clustering;
+
+
+//
+//void Cluster::__del() {
+//
+//}
+//
+//void Cluster::__cpy(LNodePtr pts) {
+//
+//}
+//
+//bool Cluster::__in(const Point &p) const {
+//    return false;
+//}
+//
+//Cluster::Cluster() {
+//
+//}
+//
+//Cluster::Cluster(const Cluster &cluster) {
+//
+//}
+//
+//Cluster &Cluster::operator=(const Cluster &cluster) {
+//    return <#initializer#>;
+//}
+//
+//
+//Cluster::~Cluster() {
+//
+//}
+//
+//int Cluster::getSize() const {
+//    return 0;
+//}
+//
+//void Cluster::add(const Point &point) {
+//
+//}
+//
+//const Point &Cluster::remove(const Point &point) {
+//    return <#initializer#>;
+//}
+//
+//bool Cluster::contains(const Point &point) {
+//    return false;
+//}
+//
+//const Point &Cluster::operator[](unsigned int index) const {
+//    return <#initializer#>;
+//}
+//
+//Cluster &Cluster::operator+=(const Point &point) {
+//    return <#initializer#>;
+//}
+//
+//Cluster &Cluster::operator-=(const Point &point) {
+//    return <#initializer#>;
+//}
+//
